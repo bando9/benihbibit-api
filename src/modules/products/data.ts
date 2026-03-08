f3a7030f5bb4d05f69e146dead22f895d8e7f51d
bando9 <bandomega1@gmail.com>
2026-03-08 16:36:05 +0700

    chore: add data

--- src/modules/products/data.ts
+++ src/modules/products/data.ts
@@ -2,69 +2,235 @@ import { SeedProductsType } from "./schema-type";
 
 export const products: SeedProductsType = [
   {
-    slug: "benih-lokal-organik-cosmos-20-biji",
-    name: "Benih Lokal Organik Cosmos 20 biji",
-    sku: "BEN-COS-1",
+    slug: "benih-cosmos-20-biji",
+    name: "Benih Cosmos 20 Biji",
+    sku: "BEN-COS-01",
     price: 5700,
     stockQuantity: 10,
     imageUrl:
       "https://27hpovti1s.ucarecd.net/4a8d4699-a3b8-47fe-9112-bc75d635d751/-/preview/639x629/",
     description:
-      "Menyediakan benih buah dan bunga lokal organik dari petani Kampung Eduwisata Areng Lembang. Benih dikemas dengan ziplock foil. Isi 1gram",
+      "Benih bunga Cosmos dengan warna cerah dan mudah tumbuh. Cocok untuk taman rumah dan pot dekoratif. Isi 20 biji dalam kemasan ziplock foil.",
   },
   {
     slug: "benih-hibiscus-merah-tropis-15-biji",
     name: "Benih Hibiscus Merah Tropis 15 Biji",
-    sku: "BEN-HIB-1",
-    price: 12000,
+    sku: "BEN-HIB-01",
+    price: 5700,
     stockQuantity: 25,
     imageUrl:
       "https://27hpovti1s.ucarecd.net/09f3670c-8d01-47eb-ab88-8dcf3c41dc15/hibiscus.png",
     description:
-      "Benih bunga Hibiscus merah tropis berkualitas tinggi. Cocok ditanam di iklim Indonesia, menghasilkan bunga besar dan cerah. Kemasan ziplock foil, isi 15 biji.",
+      "Benih bunga Hibiscus merah tropis berkualitas tinggi. Cocok ditanam di iklim Indonesia dengan bunga besar dan warna cerah. Isi 15 biji.",
   },
   {
     slug: "benih-marigold-kuning-cerah-20-biji",
     name: "Benih Marigold Kuning Cerah 20 Biji",
-    sku: "BEN-MAR-1",
-    price: 8500,
+    sku: "BEN-MAR-01",
+    price: 5700,
     stockQuantity: 40,
     imageUrl:
       "https://27hpovti1s.ucarecd.net/74117fa4-f0c7-456f-bac8-bf96061ab885/marigold.png",
     description:
-      "Benih bunga Marigold warna kuning cerah dengan daya tumbuh tinggi. Cocok untuk taman rumah dan tanaman pagar alami. Isi 20 biji, kemasan higienis.",
+      "Benih bunga Marigold warna kuning cerah dengan daya tumbuh tinggi. Cocok untuk taman rumah dan tanaman pagar alami. Isi 20 biji.",
   },
   {
     slug: "benih-daisy-putih-klasik-25-biji",
     name: "Benih Daisy Putih Klasik 25 Biji",
-    sku: "BEN-DAI-1",
-    price: 9000,
+    sku: "BEN-DAI-01",
+    price: 5700,
     stockQuantity: 30,
     imageUrl:
       "https://27hpovti1s.ucarecd.net/73676660-0725-462b-95e5-61afabfded68/daisy.png",
     description:
-      "Benih Daisy putih klasik dengan kelopak bersih dan pusat kuning cerah. Mudah ditanam untuk pemula. Isi 25 biji, cocok untuk taman minimalis.",
+      "Benih Daisy putih klasik dengan kelopak bersih dan pusat kuning cerah. Mudah ditanam untuk pemula. Isi 25 biji.",
   },
   {
     slug: "benih-larkdaisy-mix-color-20-biji",
     name: "Benih Larkdaisy Mix Color 20 Biji",
-    sku: "BEN-LKD-1",
-    price: 10000,
+    sku: "BEN-LKD-01",
+    price: 5700,
     stockQuantity: 20,
     imageUrl:
       "https://27hpovti1s.ucarecd.net/d5bb179d-ecd6-4e8a-86d6-1b5d6d56bfad/lardkdaisy.png",
     description:
-      "Benih Larkdaisy campuran warna cerah (ungu, pink, putih). Cocok untuk mempercantik taman atau pot dekoratif. Isi 20 biji, daya tumbuh optimal.",
+      "Benih Larkdaisy campuran warna cerah seperti ungu, pink, dan putih. Cocok untuk taman atau pot dekoratif. Isi 20 biji.",
   },
   {
     slug: "benih-zinnia-mix-color-30-biji",
     name: "Benih Zinnia Mix Color 30 Biji",
-    sku: "BEN-ZIN-1",
-    price: 9500,
+    sku: "BEN-ZIN-01",
+    price: 5700,
     stockQuantity: 35,
     imageUrl:
       "https://27hpovti1s.ucarecd.net/28e095f4-c352-4e9e-bd36-de7439cc3151/zinnia.png",
     description:
-      "Benih Zinnia warna-warni dengan bunga besar dan tahan panas. Cocok untuk taman tropis Indonesia. Isi 30 biji dalam kemasan foil ziplock.",
+      "Benih Zinnia warna-warni dengan bunga besar dan tahan panas. Cocok untuk taman tropis Indonesia. Isi 30 biji.",
+  },
+
+  {
+    slug: "benih-seledri-20-biji",
+    name: "Benih Seledri 20 Biji",
+    sku: "BEN-SEL-01",
+    price: 5700,
+    stockQuantity: 35,
+    imageUrl:
+      "https://27hpovti1s.ucarecd.net/b705dc0b-dcda-426d-9a78-f0955c724960/seledri.png",
+    description:
+      "Benih seledri berkualitas dengan pertumbuhan cepat. Cocok ditanam di pot atau kebun rumah. Isi 20 biji.",
+  },
+  {
+    slug: "benih-cabe-cengek-20-biji",
+    name: "Benih Cabe Cengek 20 Biji",
+    sku: "BEN-CAB-01",
+    price: 1000,
+    stockQuantity: 35,
+    imageUrl:
+      "https://27hpovti1s.ucarecd.net/17702c2f-ac4c-4f7e-978f-4f34985bf1ba/cabecengek.png",
+    description:
+      "Benih cabe cengek pedas khas Indonesia. Cocok ditanam di pot atau kebun rumah. Isi 20 biji.",
+  },
+  {
+    slug: "benih-ketumbar-20-biji",
+    name: "Benih Ketumbar 20 Biji",
+    sku: "BEN-KET-01",
+    price: 10000,
+    stockQuantity: 35,
+    imageUrl:
+      "https://27hpovti1s.ucarecd.net/d6f65c48-8a38-4663-97d3-cc9b66093208/ketumbar.png",
+    description:
+      "Benih ketumbar berkualitas untuk tanaman rempah dapur. Mudah ditanam dan cocok untuk kebun rumah.",
+  },
+
+  {
+    slug: "benih-lollipop-flower",
+    name: "Benih Lollipop Flower",
+    sku: "BEN-LOP-01",
+    price: 12000,
+    stockQuantity: 35,
+    imageUrl:
+      "https://27hpovti1s.ucarecd.net/380214c5-a68e-4a9b-8d92-236c7e30b78d/lolipop.png",
+    description:
+      "Benih bunga Lollipop dengan bentuk unik dan warna menarik. Cocok untuk mempercantik taman.",
+  },
+  {
+    slug: "benih-selada-green-romaine",
+    name: "Benih Selada Green Romaine",
+    sku: "BEN-ROM-01",
+    price: 1000,
+    stockQuantity: 35,
+    imageUrl:
+      "https://27hpovti1s.ucarecd.net/bae655af-1f02-4468-aab5-3de8292352cb/romaine.png",
+    description:
+      "Benih selada Green Romaine segar dan mudah ditanam. Cocok untuk kebun rumah dan hidroponik.",
+  },
+  {
+    slug: "benih-zebrina-gantung",
+    name: "Benih Zebrina Gantung",
+    sku: "BEN-ZEB-01",
+    price: 7000,
+    stockQuantity: 35,
+    imageUrl:
+      "https://27hpovti1s.ucarecd.net/e3c9b634-e8b0-434a-8a12-ccecc2c09fe3/zebrinagantung.png",
+    description:
+      "Benih tanaman Zebrina gantung dengan daun dekoratif. Cocok untuk pot gantung dan taman minimalis.",
+  },
+  {
+    slug: "benih-lengkuas",
+    name: "Benih Lengkuas",
+    sku: "BEN-LEN-01",
+    price: 7500,
+    stockQuantity: 35,
+    imageUrl:
+      "https://27hpovti1s.ucarecd.net/3d199306-89ff-426a-b906-b9f51ebafe90/lengkuas.png",
+    description:
+      "Benih lengkuas untuk tanaman rempah dapur. Cocok ditanam di kebun rumah.",
+  },
+  {
+    slug: "benih-mint",
+    name: "Benih Mint",
+    sku: "BEN-MIN-01",
+    price: 8200,
+    stockQuantity: 35,
+    imageUrl:
+      "https://27hpovti1s.ucarecd.net/d764fa2c-354a-4108-978f-f8acb83ae248/mint.png",
+    description:
+      "Benih tanaman mint segar dengan aroma khas. Cocok untuk kebun herbal rumah.",
+  },
+  {
+    slug: "benih-parsley",
+    name: "Benih Parsley",
+    sku: "BEN-PAR-01",
+    price: 10000,
+    stockQuantity: 35,
+    imageUrl:
+      "https://27hpovti1s.ucarecd.net/121e9eec-9f23-4c7a-8c32-75924d688e65/parsley.png",
+    description: "Benih parsley segar yang cocok untuk tanaman herbal dapur.",
+  },
+  {
+    slug: "benih-jahe-merah",
+    name: "Benih Jahe Merah",
+    sku: "BEN-JAH-01",
+    price: 7500,
+    stockQuantity: 35,
+    imageUrl:
+      "https://27hpovti1s.ucarecd.net/f2745d83-e4a3-4079-86d5-fadabcf7888f/jahemerah.png",
+    description:
+      "Benih jahe merah dengan manfaat kesehatan dan cocok ditanam di kebun rumah.",
+  },
+  {
+    slug: "benih-kelor",
+    name: "Benih Kelor",
+    sku: "BEN-KEL-01",
+    price: 12000,
+    stockQuantity: 35,
+    imageUrl:
+      "https://27hpovti1s.ucarecd.net/b6346970-ac87-43f0-bef0-de4bee04af2e/kelor.png",
+    description:
+      "Benih tanaman kelor yang dikenal dengan manfaat kesehatan tinggi.",
+  },
+  {
+    slug: "benih-pandan",
+    name: "Benih Pandan",
+    sku: "BEN-PAN-01",
+    price: 9500,
+    stockQuantity: 35,
+    imageUrl:
+      "https://27hpovti1s.ucarecd.net/e7310924-8f91-47b2-a9ed-f290fac98d43/pandan.png",
+    description:
+      "Benih pandan harum yang cocok untuk tanaman dapur dan bumbu masakan.",
+  },
+  {
+    slug: "benih-wortel",
+    name: "Benih Wortel",
+    sku: "BEN-WOR-01",
+    price: 5700,
+    stockQuantity: 35,
+    imageUrl:
+      "https://27hpovti1s.ucarecd.net/919c502b-0e37-414f-bd8b-dc518a886100/wortel.png",
+    description:
+      "Benih wortel segar dengan pertumbuhan cepat dan cocok untuk kebun rumah.",
+  },
+  {
+    slug: "benih-pakcoy-20-biji",
+    name: "Benih Pakcoy 20 Biji",
+    sku: "BEN-PAK-01",
+    price: 5700,
+    stockQuantity: 35,
+    imageUrl:
+      "https://27hpovti1s.ucarecd.net/fb3679f5-189b-40ae-a0a0-d33cf8b43a25/pakcoy.png",
+    description:
+      "Benih pakcoy segar yang cocok ditanam di kebun rumah atau sistem hidroponik.",
+  },
+  {
+    slug: "benih-katuk",
+    name: "Benih Katuk",
+    sku: "BEN-KAT-01",
+    price: 1200,
+    stockQuantity: 35,
+    imageUrl:
+      "https://27hpovti1s.ucarecd.net/a75e34f8-98f2-4d30-84ec-03acc8be2b74/katuk.png",
+    description:
+      "Benih tanaman katuk yang dikenal sebagai sayuran bergizi tinggi.",
   },
 ];
