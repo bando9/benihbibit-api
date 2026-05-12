e09c5efc49cd8f336881ab5f9f1d509f0e41a889
bando9 <bandomega1@gmail.com>
2026-05-12 23:16:30 +0700

    feat: totalPrice & subTotalPrice

---
+++ prisma/migrations/20260512123511_total_and_subtotal_price/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Cart" ADD COLUMN     "totalPrice" INTEGER NOT NULL DEFAULT 0;
+
+-- AlterTable
+ALTER TABLE "CartItem" ADD COLUMN     "subTotalPrice" INTEGER NOT NULL DEFAULT 0,
+ALTER COLUMN "quantity" SET DEFAULT 1;
